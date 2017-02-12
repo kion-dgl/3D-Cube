0f98bd488955d6edfba1c64a0521dc867c72974a
Benjamin Collins <kion@dashgl.com>
2017-02-12 14:39:05 +0900

    using tabs

--- 01_window/main.c
+++ 01_window/main.c
@@ -1,19 +1,19 @@
 /*
-    This file is part of DashGL.com OpenGL 2.0 Introduction
+	This file is part of DashGL.com OpenGL 2.0 Introduction
 
-    Copyright (C) 2017 Benjamin Collins
+	Copyright (C) 2017 Benjamin Collins
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License version 2
-    as published by the Free Software Foundation.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License version 2
+	as published by the Free Software Foundation.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <stdio.h>
